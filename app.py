40d767da3ecdf5e46e6936ae8db97561fe89b84b
priyarai121 <priyarai12169@gmail.com>
2026-05-12 13:27:53 +0530

    Step 3: Error Handling

--- app.py
+++ app.py
@@ -283,5 +283,13 @@ def history():
     
     return render_template('history.html', documents=verified_docs)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
